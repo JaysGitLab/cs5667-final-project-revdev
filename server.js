442926187050f3fed0ac087b23140c0ae9ebb3f9
Michelle Melton <meltonml@appstate.edu>
2018-04-15 17:55:23 -0400

    Set up initial application config files

---
+++ server.js
@@ -0,0 +1,11 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const configureMongoose = require('./config/mongoose');
+const configureExpress = require('./confg/express');
+const db = configureMongoose();
+const app = configureExpress();
+
+app.listen(3000);
+module.exports = app;
+
+console.log('Server running at http://localhost:3000');
