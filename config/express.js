6e5efa1ecdab3f42d5fbaddc259fadc4b92e226e
Michelle Melton <meltonml@appstate.edu>
2018-04-16 08:49:14 -0400

    Fix missing paren

--- config/express.js
+++ config/express.js
@@ -33,7 +33,7 @@ module.exports = function() {
   require('../app/routes/index.server.routes.js')(app);
   require('../app/routes/users/server.routes.js')(app);
 
-  app.use(express.static('./public');
+  app.use(express.static('./public'));
 
   return app;
 };
