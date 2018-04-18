c017d262f86689373735087d85c8bef7e35d0b61
Michelle Melton <meltonml@appstate.edu>
2018-04-18 11:03:28 -0400

    Add passport to main server file

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
+const configurePassport = require('./config/passport');
 const db = configureMongoose();
 const app = configureExpress();
+const passport = configurePassport();
 
 app.listen(3000);
 module.exports = app;
