b10a0c3d1546adab494a0c118dcf0f06127bf5d9
Michelle Melton <meltonml@appstate.edu>
2018-04-19 18:43:09 -0400

    Fix error in index controller

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -3,7 +3,7 @@ exports.render = function (req, res) {
   // Use the 'response' object to render the 'index' view with a 'title' and 'userFullName' properties
   res.render('index', {
     title: 'Welcome to Green Valley Community Park',
-    headline: 'Reservation System'
-    userFullName: req.user ? req.user : '',
+    headline: 'Reservation System',
+    userFullName: req.user ? req.user : ''
   });
 };
