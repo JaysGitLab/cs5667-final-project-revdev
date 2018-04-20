238bb2546e68d0456e9f60a65c534d1f3195e49a
Michelle Melton <meltonml@appstate.edu>
2018-04-19 21:06:48 -0400

    Fix local strategy, change email to username for passport

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -3,9 +3,9 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('mongoose').model('User');
 
 module.exports = function() {
-  passport.use(new LocalStrategy((email, password, done) => {
+  passport.use(new LocalStrategy((username, password, done) => {
     User.findOne({
-      email: email 
+      username: username 
     }, (err, user) => {
       if (err) {
         return done(err);
