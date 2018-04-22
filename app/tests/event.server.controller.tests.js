ee1109a5aaa7727f07b8214fa20b6a04bb24ef9d
Cydney Caldwell <caldwellc1@appstate.edu>
2018-04-22 16:43:59 -0400

    controller and test

---
+++ app/tests/event.server.controller.tests.js
@@ -0,0 +1,48 @@
+const app = require('../../server');
+const request = require('supertest');
+const should = require('should');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Event = mongoose.model('Event');
+
+let user;
+
+
+describe('Event Controller Unit Tests:', () => {
+      beforeEach((done) => {
+        user = new User({
+          firstName: 'Full',
+          lastName: 'Name',
+          phone: '123-456-7890',
+          displayName: 'Full Name',
+          username: 'test@test.com',
+          password: 'password',
+          provider: 'local',
+        });
+        eventName = new Event({
+            eventType: 'Birthday',
+            numberOfPeopleFrom: 4,
+            numberOfPeopleTo: 2,
+            cost: 25,
+            deposit: 25,
+            reminderEmail: 1,
+            freeCancelation: 1,
+            maxNumberOfDays: 2,            
+        });
+        user.save(() => {});
+        done();
+      });
+      describe('Test to create.', () => {
+          it('Should be able to create a new event.', () => {
+              event.create((err) => {
+                  should.exist(err);
+              });
+          });    
+      });
+      afterEach((done) => {
+          Event.remove(() => {
+              done();
+          });
+      });
+});
+
