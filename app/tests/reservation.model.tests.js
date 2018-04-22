5a3c42bfba1d311810931610b3cc261b3212b579
patrickbeekman <patrickbeekman1095@gmail.com>
2018-04-22 16:31:54 -0400

    added tests to check for the date validation I just commited

--- app/tests/reservation.model.tests.js
+++ app/tests/reservation.model.tests.js
@@ -12,7 +12,7 @@ let reservation;
 describe('Reservation Model Unit Tests:', () => {
     // Create new event object
     beforeEach((done) => {
-        res = new reservation({
+        reservation = new Reservation({
             email: 'beekmanpc@appstate.edu',
             dateCreated: new Date('April 18, 2018 00:00:00');,
             startTime: new Date('April 18, 2018 01:00:00');,
@@ -83,7 +83,43 @@ describe('Reservation Model Unit Tests:', () => {
                 should.exist(err);
             });
         });
-        
+
+        // Test 7 checks model validation for reservation endTime before startTime
+        it('Should not be able to save a reservation with the endTime before startTime', () => {
+            reservation.startTime = new Date('April 18, 2018 00:00:00');
+            reservation.endTime = new Date('April 10, 2018 00:00:00');
+            reservation.save((err) => {
+                should.exist(err);
+            });
+        });
+
+        // Test 8 checks model validation for reservation endTime is within maxNumberOfDays for eventType
+        it('Should not be able to save a reservation with the endTime before startTime', () => {
+        	reservation.eventType = new Event({
+	            eventType: 'Birthday Party',
+	            numberOfPeopleFrom: 0,
+	            numberOfPeopleTo: 30,
+	            cost: 25,
+	            deposit: 0,
+	            reminderEmail: 3,
+	            freeCancelation: 5,
+	            maxNumberOfDays: 1
+        	});
+            reservation.startTime = new Date('April 18, 2018 00:00:00');
+            // 3 days later when maxNumberOfDays should only be 1 day so April 19th
+            reservation.endTime = new Date('April 21, 2018 00:00:00');
+            reservation.save((err) => {
+                should.exist(err);
+            });
+        });
+
     });
+
+    // Clean up User collection
+  	afterEach((done) => {
+    	reservation.remove(() => {
+        	done();
+    	});
+  	});
 
 });
