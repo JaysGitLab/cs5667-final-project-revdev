4ba7f239f81645ae414f6b218fe28343ce5a8264
Benjamin Wascher <wascherb@appstate.edu>
2018-04-24 14:22:48 -0400

    implemented email module

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,9 +1,20 @@
+const nodemail = require('../libs/mail');
+const config = require('../../config/config');
+
+var mailOptions = {
+  from: config.email,
+  to: config.email,
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
 // Create a new 'render' controller method
 exports.render = function (req, res) {
   // Use the 'response' object to render the 'index' view with a 'title' and 'userFullName' properties
   res.render('index', {
     title: 'Welcome to Green Valley Community Park',
     headline: 'Reservation System',
-    userFullName: req.user ? req.user.fullName : ''
+    userFullName: req.user ? req.user.fullName : '',
+    test: nodemail.sendMail(mailOptions)
   });
 };
