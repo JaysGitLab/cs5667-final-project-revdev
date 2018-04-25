0bd258a895774f28eecf7d1083902bf71a45db48
Michelle Melton <meltonml@appstate.edu>
2018-04-25 10:09:18 -0400

    Start create reservation view

---
+++ app/views/createRes.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>
+      <%=title %>
+  </title>
+</head>
+<body>
+  <% for(var i in messages) { %>
+    <div class="flash"><%=messages[i] %></div>
+  <% } %>
+  <form action="/createRes" method="post">
+    <div>
+      <label>Email:</label>
+      <input type="text" name="username" value="<%=user.username %>" />
+    </div>
+    <div>
+      <label>Start Date/Time:</label>
+      <input type="datetime-local" name="startTime" />
+    </div>
+    <div>
+      <label>End Date/Time:</label>
+      <input type="datetime-local" name="endTime" />
+    </div>
+    <div>
+      <label>Area(s) Needed:</label>
+      <input type="checkbox" name="areas" value="Picnic shelter" />
+      <input type="checkbox" name="areas" value="Lower field" />
+    </div>
+    <div>
+      <label>Purpose:</label>
+      <input type="" name="eventType" />
+    </div>
+    <div>
+      <label>Other Relevant Information:</label>
+      <input type="textarea" name="comments" />
+    </div>
+    <div>
+      <input type="submit" value="Save" />
+    </div>
+  </form>
+</body>
+</html>
